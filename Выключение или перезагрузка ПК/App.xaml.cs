bc4d4b6c37a9c4c247cbd5b5ddc4e1e61c21bb9a
Ilya <124510757+DevIsaev@users.noreply.github.com>
2023-05-17 21:32:33 +0700

    Добавлена программа выключения или перезагрузки

---
+++ Выключение или перезагрузка ПК/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Выключение_или_перезагрузка_ПК
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Выключение или перезагрузка ПК/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Выключение_или_перезагрузка_ПК
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShutDown_Click(object sender, RoutedEventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "shutdown.exe";
+            p.StartInfo.Arguments = "/s /t 0";// /s резко /t быстро
+            p.Start();
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+           Process p = new Process();
+            p.StartInfo.FileName = "shutdown.exe";
+            p.StartInfo.Arguments = "/r /t 0";
+            p.Start();
+        }
+    }
+}
